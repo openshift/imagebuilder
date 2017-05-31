a87373954e7f63241f7b653471c732adcd9e50ce
Jim Minter <jminter@redhat.com>
2017-05-31 15:13:49 +0100

    cleanup goroutine properly in imageprogress.NewPullWriter

--- imageprogress/progress_test.go
+++ imageprogress/progress_test.go
@@ -10,10 +10,9 @@ import (
 
 func TestReports(t *testing.T) {
 	tests := []struct {
-		name        string
-		gen         func(*progressGenerator)
-		errExpected bool
-		expected    report
+		name     string
+		gen      func(*progressGenerator)
+		expected report
 	}{
 		{
 			name: "simple report",
@@ -53,12 +52,17 @@ func TestReports(t *testing.T) {
 			},
 		},
 		{
-			name: "detect error",
+			name: "ignore error",
 			gen: func(p *progressGenerator) {
+				p.status("1", "Downloading")
+				p.status("2", "Pull complete")
 				p.status("1", "Downloading")
 				p.err("an error")
 			},
-			errExpected: true,
+			expected: report{
+				statusDownloading: &layerDetail{Count: 1},
+				statusComplete:    &layerDetail{Count: 1},
+			},
 		},
 	}
 
@@ -81,46 +85,12 @@ func TestReports(t *testing.T) {
 		w.(*imageProgressWriter).stableThreshhold = 0
 		_, err := io.Copy(w, pipeIn)
 		if err != nil {
-			if !test.errExpected {
-				t.Errorf("%s: unexpected: %v", test.name, err)
-			}
-			continue
-		}
-		if test.errExpected {
-			t.Errorf("%s: did not get expected error", test.name)
+			t.Errorf("%s: unexpected: %v", test.name, err)
 			continue
 		}
 		if !compareReport(lastReport, test.expected) {
 			t.Errorf("%s: unexpected report, got: %v, expected: %v", test.name, lastReport, test.expected)
 		}
-	}
-}
-
-func TestErrorOnCopy(t *testing.T) {
-	// Producer pipe
-	genIn, genOut := io.Pipe()
-	p := newProgressGenerator(genOut)
-
-	// generate some data
-	go func() {
-		for i := 0; i < 100; i++ {
-			p.status("1", "Downloading")
-			p.status("2", "Downloading")
-			p.status("3", "Downloading")
-		}
-		p.err("data error")
-		genOut.Close()
-	}()
-
-	w := newWriter(func(r report) {}, func(a, b report) bool { return true })
-
-	// Ensure that the error is propagated to the copy
-	_, err := io.Copy(w, genIn)
-	if err == nil {
-		t.Errorf("Did not get an error when copying to writer")
-	}
-	if err.Error() != "data error" {
-		t.Errorf("Did not get expected error: %v", err)
 	}
 }
 
@@ -161,10 +131,10 @@ func TestStableLayerCount(t *testing.T) {
 	for _, test := range tests {
 		w := newWriter(func(report) {}, func(a, b report) bool { return true }).(*imageProgressWriter)
 		w.lastLayerCount = test.lastLayerCount
-		w.layerStatus = map[string]progressLine{}
+		w.layerStatus = map[string]*progressLine{}
 		w.stableThreshhold = test.stableThreshhold
 		for i := 0; i < test.layerStatusCount; i++ {
-			w.layerStatus[strconv.Itoa(i)] = progressLine{}
+			w.layerStatus[strconv.Itoa(i)] = &progressLine{}
 		}
 		var result bool
 		for i := 0; i < test.callCount; i++ {
