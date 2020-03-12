e8e362885eecd2c7c222371aba1bd3b5b717b8d5
Daniel J Walsh <dwalsh@redhat.com>
2020-03-12 15:44:42 -0400

    Add tests to verify copy and add with environment substitution.
    Also removed an unecessary check for length of flagArgs list.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- dispatchers.go
+++ dispatchers.go
@@ -153,18 +153,16 @@ func add(b *Builder, args []string, attributes map[string]bool, flagArgs []strin
 	var chown string
 	last := len(args) - 1
 	dest := makeAbsolute(args[last], b.RunConfig.WorkingDir)
-	if len(flagArgs) > 0 {
-		for _, a := range flagArgs {
-			arg, err := ProcessWord(a, b.Env)
-			if err != nil {
-				return err
-			}
-			switch {
-			case strings.HasPrefix(arg, "--chown="):
-				chown = strings.TrimPrefix(arg, "--chown=")
-			default:
-				return fmt.Errorf("ADD only supports the --chown=<uid:gid> flag")
-			}
+	for _, a := range flagArgs {
+		arg, err := ProcessWord(a, b.Env)
+		if err != nil {
+			return err
+		}
+		switch {
+		case strings.HasPrefix(arg, "--chown="):
+			chown = strings.TrimPrefix(arg, "--chown=")
+		default:
+			return fmt.Errorf("ADD only supports the --chown=<uid:gid> flag")
 		}
 	}
 	b.PendingCopies = append(b.PendingCopies, Copy{Src: args[0:last], Dest: dest, Download: true, Chown: chown})
@@ -183,20 +181,18 @@ func dispatchCopy(b *Builder, args []string, attributes map[string]bool, flagArg
 	dest := makeAbsolute(args[last], b.RunConfig.WorkingDir)
 	var chown string
 	var from string
-	if len(flagArgs) > 0 {
-		for _, a := range flagArgs {
-			arg, err := ProcessWord(a, b.Env)
-			if err != nil {
-				return err
-			}
-			switch {
-			case strings.HasPrefix(arg, "--chown="):
-				chown = strings.TrimPrefix(arg, "--chown=")
-			case strings.HasPrefix(arg, "--from="):
-				from = strings.TrimPrefix(arg, "--from=")
-			default:
-				return fmt.Errorf("COPY only supports the --chown=<uid:gid> and the --from=<image|stage> flags")
-			}
+	for _, a := range flagArgs {
+		arg, err := ProcessWord(a, b.Env)
+		if err != nil {
+			return err
+		}
+		switch {
+		case strings.HasPrefix(arg, "--chown="):
+			chown = strings.TrimPrefix(arg, "--chown=")
+		case strings.HasPrefix(arg, "--from="):
+			from = strings.TrimPrefix(arg, "--from=")
+		default:
+			return fmt.Errorf("COPY only supports the --chown=<uid:gid> and the --from=<image|stage> flags")
 		}
 	}
 	b.PendingCopies = append(b.PendingCopies, Copy{From: from, Src: args[0:last], Dest: dest, Download: false, Chown: chown})
