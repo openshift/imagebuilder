35d69a2bb2fca1a940c111f59e24ecf07ca76fb4
Nalin Dahyabhai <nalin@redhat.com>
2020-09-10 11:55:50 -0400

    Bump back to v1.2-dev
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- cmd/imagebuilder/imagebuilder.go
+++ cmd/imagebuilder/imagebuilder.go
@@ -31,7 +31,7 @@ func main() {
 	var version bool
 	var mountSpecs stringSliceFlag
 
-	VERSION := "1.1.7"
+	VERSION := "1.2-dev"
 	arguments := stringMapFlag{}
 
 	flag.Var(&tags, "t", "The name to assign this image, if any. May be specified multiple times.")
