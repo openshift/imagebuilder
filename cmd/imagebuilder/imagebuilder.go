69956215233e05995b66f9008a922496da812ae7
Daniel J Walsh <dwalsh@redhat.com>
2021-02-22 16:19:06 -0500

    Move to v1.2.1-dev
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- cmd/imagebuilder/imagebuilder.go
+++ cmd/imagebuilder/imagebuilder.go
@@ -31,7 +31,7 @@ func main() {
 	var version bool
 	var mountSpecs stringSliceFlag
 
-	VERSION := "1.2.0"
+	VERSION := "1.2.1-dev"
 	arguments := stringMapFlag{}
 
 	flag.Var(&tags, "t", "The name to assign this image, if any. May be specified multiple times.")
