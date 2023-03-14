aa8855f2a994fd0cd694358c23aa1f05ac1479eb
Nalin Dahyabhai <nalin@redhat.com>
2023-03-14 17:57:12 -0400

    Tag v1.2.4
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- cmd/imagebuilder/imagebuilder.go
+++ cmd/imagebuilder/imagebuilder.go
@@ -32,7 +32,7 @@ func main() {
 	var version bool
 	var mountSpecs stringSliceFlag
 
-	VERSION := "1.2.4-dev"
+	VERSION := "1.2.4"
 	arguments := stringMapFlag{}
 
 	flag.Var(&tags, "t", "The name to assign this image, if any. May be specified multiple times.")
