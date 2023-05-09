64665d8f9b92948fd96b0a61172862d164706115
Nalin Dahyabhai <nalin@redhat.com>
2023-05-09 16:42:13 -0400

    Record our version as v1.2.6-dev
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- cmd/imagebuilder/imagebuilder.go
+++ cmd/imagebuilder/imagebuilder.go
@@ -32,7 +32,7 @@ func main() {
 	var version bool
 	var mountSpecs stringSliceFlag
 
-	VERSION := "1.2.5"
+	VERSION := "1.2.6-dev"
 	arguments := stringMapFlag{}
 
 	flag.Var(&tags, "t", "The name to assign this image, if any. May be specified multiple times.")
