ffb82e2dd2dad3e142c77bb2b1095f56a570d4a5
Nalin Dahyabhai <nalin@redhat.com>
2024-06-03 13:33:28 -0400

    Tag v1.2.10
    Update version information to mark this as v1.2.10.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- cmd/imagebuilder/imagebuilder.go
+++ cmd/imagebuilder/imagebuilder.go
@@ -32,7 +32,7 @@ func main() {
 	var version bool
 	var mountSpecs stringSliceFlag
 
-	VERSION := "1.2.10-dev"
+	VERSION := "1.2.10"
 	arguments := stringMapFlag{}
 
 	flag.Var(&tags, "t", "The name to assign this image, if any. May be specified multiple times.")
